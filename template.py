ddf6c5506c82dd85363aa124b6e265c1778fd733
JanvitaReddy11 <tamu2023@tamu.edu>
2024-06-07 18:07:22 -0500

    Folder structure operated

---
+++ template.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+ ## Automatically take care of system path (wondows, linux)
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/util.py",
+    "src/logger/logging.py",
+    "src/exception/exception"
+    "tests/unit/__init__.py",
+     "tests/intergration/__init__.py",
+     "init__setup.sh",
+     "requirements.txt",
+     "requirements_dev.txt",
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+    "expirement/experiments.ipynb",
+
+
+
+]
+
+for filepath in list_of_files:
+    file_name = Path(filepath)
+    filedir,file = os.path.split(file_name)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok = True)
+    if (not os.path.exists(file_name)) or (os.path.getsize(file_name) == 0):
+        with open(file_name, "w") as f:
+            pass 
